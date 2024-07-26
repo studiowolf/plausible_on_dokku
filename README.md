e55b8bb2cd94cbafbc8d99e5e42bb831026fa80e
Tim Sluis <tim@timsluis.nl>
2024-07-26 10:27:56 +0200

    Improved backup script and cron

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Plausible on Dokku
+### Plausible on Dokku
 
 Based on Plausible on Dokku (https://github.com/d1ceward/plausible_on_dokku/blob/master/Dockerfile)
 Check for latest version
@@ -6,3 +6,7 @@ Check for latest version
 # Postgres certificate
 
 postgres.crt should be replaced by Sunday, 25 May 2031. It can be downloaded from the DigitalOcean console.
+
+# Backup
+
+For now we run a FULL clickhouse backup once every three days at 3 am. Clickhouse backups could become pretty large in teh future. In that case we should start using incremental backups (and only make a full new backup once a month or so).
---
+++ backup_clickhouse.py
@@ -0,0 +1,33 @@
+import os
+from datetime import datetime
+from clickhouse_driver import Client
+
+# Replace these with your actual connection details
+connection_string = os.getenv('CLICKHOUSE_DATABASE_URL')
+s3_access_key_id = os.getenv('B2_ACCESS_KEY_ID')
+s3_secret_access_key = os.getenv('B2_SECRET_ACCESS_KEY')
+
+if not all([connection_string, s3_access_key_id, s3_secret_access_key]):
+    raise ValueError("Missing one or more required environment variables.")
+
+# Create a client instance using the connection string
+client = Client.from_url(connection_string)
+
+# Generate a timestamp
+timestamp = datetime.now().strftime('%Y-%m-%d-%H:%M')
+
+# Define the BACKUP query with environment variable credentials
+backup_query = f"""
+BACKUP DATABASE plausible_events_db TO S3(
+    'https://s3.eu-central-003.backblazeb2.com/wolfmaps-backup/clickhouse/{timestamp}/',
+    '{s3_access_key_id}',
+    '{s3_secret_access_key}'
+)
+"""
+
+# Execute the query
+try:
+    result = client.execute(backup_query)
+    print("Backup completed successfully.")
+except Exception as e:
+    print("An error occurred:", e)
